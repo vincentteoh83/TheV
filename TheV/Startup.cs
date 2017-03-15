44c503604bd225f521cf18713dc2f838c3f76fb5
vincent.teoh <vincent.teoh@MADT26>
2017-03-15 14:55:51 +1030

    Add project files.

---
+++ TheV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheV.Startup))]
+namespace TheV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
